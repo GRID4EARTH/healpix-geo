8fad3ba4384083c4cd762a997dd0d4dcbd17fe63
Justus Magin <keewis@posteo.de>
2026-06-19 11:57:47 +0200

    implement for the ring scheme

--- rust/healpix-geo-core/src/scalar/ring/coordinates.rs
+++ rust/healpix-geo-core/src/scalar/ring/coordinates.rs
@@ -1,6 +1,7 @@
 use crate::ellipsoid::{Ellipsoid, ReferenceBody};
 
 use cdshealpix as healpix;
+use cdshealpix::nested::Layer;
 
 pub fn healpix_to_lonlat(hash: &u64, nside: &u32, ellipsoid: &Ellipsoid) -> (f64, f64) {
     let center = healpix::ring::center(*nside, *hash);
@@ -31,6 +32,22 @@ pub fn vertices(hash: &u64, nside: &u32, ellipsoid: &Ellipsoid) -> Vec<(f64, f64
                 ellipsoid.latitude_authalic_to_geographic(lat).to_degrees(),
             )
         })
+        .collect()
+}
+
+pub fn bilinear_interpolation(
+    lon: &f64,
+    lat: &f64,
+    layer: &Layer,
+    ellipsoid: &Ellipsoid,
+) -> Vec<(u64, f64)> {
+    let lon_ = lon.rem_euclid(360.0).to_radians();
+    let lat_ = ellipsoid.latitude_geographic_to_authalic(lat.to_radians());
+
+    layer
+        .bilinear_interpolation(lon_, lat_)
+        .into_iter()
+        .map(|(hash, weight)| (layer.to_ring(hash), weight))
         .collect()
 }
 
--- rust/healpix-geo-core/src/vectorized/ring/coordinates.rs
+++ rust/healpix-geo-core/src/vectorized/ring/coordinates.rs
@@ -1,3 +1,4 @@
+use cdshealpix as healpix;
 #[cfg(not(target_arch = "wasm32"))]
 use rayon::prelude::*;
 
@@ -48,4 +49,61 @@ pub fn vertices(
     ));
 
     result
+}
+
+pub fn bilinear_interpolation(
+    coords: &[(f64, f64)],
+    nside: &u32,
+    ellipsoid: &Ellipsoid,
+    nthreads: usize,
+) -> Vec<Vec<(u64, f64)>> {
+    let layer = healpix::nested::get(healpix::depth(*nside));
+    let mut result = Vec::<Vec<(u64, f64)>>::with_capacity(coords.len());
+
+    maybe_parallelize!(nthreads, coords, result, |(lon, lat)| {
+        scalar::bilinear_interpolation(lon, lat, layer, ellipsoid)
+    });
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::ellipsoid::ReferenceEllipsoid;
+    use geodesy::ellps::Ellipsoid as GeodesyEllipsoid;
+
+    #[test]
+    fn test_bilinear_interpolation() {
+        let nside = 8;
+        let ellipsoid = Ellipsoid::Ellipsoid(ReferenceEllipsoid::new(
+            GeodesyEllipsoid::named("WGS84").unwrap(),
+        ));
+
+        let coords = vec![(10.0, 30.0), (45.0, 50.0), (80.0, 70.0)];
+        let nthreads: usize = 0;
+
+        let actual = bilinear_interpolation(&coords, &nside, &ellipsoid, nthreads);
+        let expected: Vec<Vec<(u64, f64)>> = vec![
+            vec![
+                (311, 0.24782825502968173),
+                (34, 0.15113311306390578),
+                (317, 0.37335533528612724),
+                (40, 0.22768329662028527),
+            ],
+            vec![
+                (48, 0.7417555426415379),
+                (49, 0.11949676803259777),
+                (50, 0.11949676803259777),
+                (51, 0.01925092129326657),
+            ],
+            vec![
+                (53, 0.47242437987207875),
+                (122, 0.06464507274912022),
+                (55, 0.4072093017055717),
+                (123, 0.05572124567322935),
+            ],
+        ];
+        assert_eq!(actual, expected);
+    }
 }
