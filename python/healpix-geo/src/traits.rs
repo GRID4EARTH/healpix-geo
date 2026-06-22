653dbd33a432914387fbdab03afe7afcfa6cc656
Justus Magin <keewis@posteo.de>
2026-06-22 16:07:27 +0200

    debug `unzip3`

--- python/healpix-geo/src/traits.rs
+++ python/healpix-geo/src/traits.rs
@@ -4,15 +4,20 @@ pub(crate) trait Unzip3<T> {
 
 impl<T> Unzip3<T> for Vec<(T, T, T)> {
     fn unzip3(self) -> (Vec<T>, Vec<T>, Vec<T>) {
-        let mut vec1 = Vec::<T>::with_capacity(self.len());
-        let mut vec2 = Vec::<T>::with_capacity(self.len());
-        let mut vec3 = Vec::<T>::with_capacity(self.len());
+        let size = self.len();
+        let mut vec1 = Vec::<T>::with_capacity(size);
+        let mut vec2 = Vec::<T>::with_capacity(size);
+        let mut vec3 = Vec::<T>::with_capacity(size);
 
-        for (index, (x, y, z)) in self.into_iter().enumerate() {
-            vec1[index] = x;
-            vec2[index] = y;
-            vec3[index] = z;
+        for (x, y, z) in self.into_iter() {
+            vec1.push(x);
+            vec2.push(y);
+            vec3.push(z);
         }
+
+        vec1.shrink_to_fit();
+        vec2.shrink_to_fit();
+        vec3.shrink_to_fit();
 
         (vec1, vec2, vec3)
     }
