3c0733b72de073ae2dbef23fc76bc4fffc138ef8
Justus Magin <keewis@posteo.de>
2026-07-07 16:53:20 +0200

    remove the unused `to_string` method

--- js/src/coordinates.rs
+++ js/src/coordinates.rs
@@ -6,11 +6,3 @@ pub struct Coordinate {
     pub lon: f64,
     pub lat: f64,
 }
-
-#[wasm_bindgen]
-impl Coordinate {
-    #[wasm_bindgen(js_name = toString)]
-    pub fn to_string(&self) -> String {
-        format!("Coordinate {{ lon: {}, lat: {} }}", self.lon, self.lat)
-    }
-}
