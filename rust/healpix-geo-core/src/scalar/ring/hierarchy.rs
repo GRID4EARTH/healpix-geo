3c08c6623b74c4268a3dcfd160896e308d71089e
Justus Magin <keewis@posteo.de>
2026-07-10 16:47:03 +0200

    convert the results to the correct indexing schemes

--- rust/healpix-geo-core/src/scalar/ring/hierarchy.rs
+++ rust/healpix-geo-core/src/scalar/ring/hierarchy.rs
@@ -5,11 +5,18 @@ pub fn kth_neighbours(hash: &u64, nside: &u32, ring: &u32) -> Vec<i64> {
     let hash_nested = layer.from_ring(*hash);
     let r = *ring;
 
-    let mut result = layer
+    let mut result: Vec<i64> = layer
         .kth_neighbours(hash_nested, r)
         .into_iter()
         .map(|v| v as i64)
-        .collect::<Vec<i64>>();
+        .map(|v| {
+            if v == -1 {
+                v
+            } else {
+                layer.to_ring(v as u64) as i64
+            }
+        })
+        .collect();
 
     // 4 sides with each 2 r + 1 values, minus 4 joints: 4 * (2r + 1) - 4
     result.resize(8 * (r as usize), -1i64);
