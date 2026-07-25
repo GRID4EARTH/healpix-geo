807afe1c8c267ec326dbf5ccc0de4a627fb2cc5b
Justus Magin <keewis@posteo.de>
2026-07-26 00:41:35 +0200

    vectorized interface

---
+++ rust/healpix-geo-core/src/vectorized/mesh.rs
@@ -0,0 +1,15 @@
+use crate::maybe_parallelize;
+#[cfg(not(target_arch = "wasm32"))]
+use rayon::prelude::*;
+
+use crate::scalar::mesh as scalar;
+
+pub fn vertex_coordinates(depth: u8, hashes: &[u64], nthreads: usize) -> Vec<(f64, f64)> {
+    let mut result = Vec::<(f64, f64)>::with_capacity(hashes.len());
+
+    maybe_parallelize!(nthreads, hashes, result, |hash| scalar::vertex_coordinates(
+        depth, hash
+    ));
+
+    result
+}
