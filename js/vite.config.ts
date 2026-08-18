7b95c56318d52973ab2028a2ed8fe692ce3b0fd9
Justus Magin <keewis@posteo.de>
2026-08-18 11:42:31 +0200

    disable the typechecks for now

--- js/vite.config.ts
+++ js/vite.config.ts
@@ -5,8 +5,9 @@ import { defineConfig } from "vite";
 export default defineConfig({
   test: {
     globals: true,
-    typecheck: {
-      enabled: true,
-    },
+    /// disabled because it causes vitest to error on Symbol.dispose in the wasm-bindgen generated code
+    // typecheck: {
+    //   enabled: true,
+    // },
   },
 });
