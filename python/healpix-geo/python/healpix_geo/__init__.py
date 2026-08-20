c957453e3e9fc7bcc5e4b5721b05c25f6edbe2e7
Justus Magin <keewis@posteo.de>
2026-08-20 16:55:02 +0200

    rename the vertex decoder to be consistent

--- python/healpix-geo/python/healpix_geo/__init__.py
+++ python/healpix-geo/python/healpix_geo/__init__.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from healpix_geo import geometry, healpix_geo, nested, ring, zuniq
 from healpix_geo.geometry import Bbox
-from healpix_geo.mesh import vertex_to_geographic
+from healpix_geo.mesh import vertex_to_lonlat
 
 
 def cartesian_to_lonlat(x, y, z, ellipsoid="sphere", num_threads=0):
@@ -99,7 +99,7 @@ __all__ = [
     "slices",
     "geometry",
     "Bbox",
-    "vertex_to_geographic",
+    "vertex_to_lonlat",
     "lonlat_to_cartesian",
     "cartesian_to_lonlat",
 ]
